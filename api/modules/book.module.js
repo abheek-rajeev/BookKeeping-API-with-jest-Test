141b1254cb10d6dfb271a012a784068542c1ea40
abheek-rajeev <abheekrajeev@gmail.com>
2024-01-25 21:41:36 +0530

    created user and book model

---
+++ api/modules/book.module.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const bookSchema= new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    isbn:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    quantity:{
+        type:Number,
+        default:0
+    }
+})
+
+const Book=mongoose.model('Book',bookSchema);
+
+export default Book;
